786cf9c51b58f8e6d9fb844c588dcc5ffffc7e87
Saalim Quadri <danascape@gmail.com>
2024-08-18 03:17:59 +0530

    start/stop-recording
    Change-Id: Ie9a6aa1d24d27c76aaee78d91e1c6074ce099894

--- app/src/main/java/org/lineageos/recorder/WidgetReceiver.java
+++ app/src/main/java/org/lineageos/recorder/WidgetReceiver.java
@@ -5,6 +5,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.util.Log;
 
+import org.lineageos.recorder.service.SoundRecorderService;
 import org.lineageos.recorder.service.WidgetService;
 
 public class WidgetReceiver extends BroadcastReceiver {
@@ -13,14 +14,15 @@ public class WidgetReceiver extends BroadcastReceiver {
         if ("START_FOREGROUND_SERVICE".equals(intent.getAction())) {
             if (WidgetService.isServiceRunning) {
                 Log.e("saalim", "WidgetService stopping");
-                Intent serviceIntent = new Intent(context, WidgetService.class);
-                serviceIntent.setAction("START_FOREGROUND_SERVICE");
+                Intent serviceIntent = new Intent(context, SoundRecorderService.class);
+                serviceIntent.setAction(SoundRecorderService.ACTION_STOP);
                 context.stopService(serviceIntent);
                 WidgetService.isServiceRunning = false;
             } else {
                 Log.e("saalim", "WidgetService Starting");
-                Intent serviceIntent = new Intent(context, WidgetService.class);
-                serviceIntent.setAction("START_FOREGROUND_SERVICE");
+                Intent serviceIntent = new Intent(context, SoundRecorderService.class);
+                serviceIntent.setAction(SoundRecorderService.ACTION_START);
+                serviceIntent.putExtra(SoundRecorderService.EXTRA_FILE_NAME, "WidgetRecorder");
                 context.startForegroundService(serviceIntent);
                 WidgetService.isServiceRunning = true;
             }
